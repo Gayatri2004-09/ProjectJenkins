ebe505924ac23202a3ad434b5e20650bc7d69a1f
Gayatri <gayatridevi@gmail.com>
2024-11-17 00:22:18 +0530

    Demo For Test

---
+++ src/main/java/MavenJavaProject/PresentProject/App.java
@@ -0,0 +1,15 @@
+package MavenJavaProject.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "K.Gayatri Devi - 22BD1A6651" );
+        
+    }
+}
